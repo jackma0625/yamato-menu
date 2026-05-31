6706ee0a232647f3c407b9398f3ce87fd444b3e0
jackma0625 <jackma0625@gmail.com>
2026-05-30 18:26:47 -0600

    update menu

---
+++ src/menu.js
@@ -0,0 +1,114 @@
+const menu = [
+
+    // ENTRADAS
+    {
+      category: "Entradas",
+      name: "Edamame",
+      price: "L.89 / 119",
+      desc: "Al vapor o asado",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Entradas",
+      name: "Miso Soup",
+      price: "L.119",
+      desc: "Sopa japonesa con tofu y algas",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Entradas",
+      name: "Gyoza",
+      price: "L.169",
+      desc: "Pollo o cerdo",
+      image: "/images/placeholder.jpg",
+    },
+  
+  
+  
+    // ROLLOS
+    {
+      category: "Rollos",
+      name: "Yamato Roll",
+      price: "L.259",
+      desc: "Camarón tempura, queso crema y aguacate",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Rollos",
+      name: "Tokyo Roll",
+      price: "L.349",
+      desc: "Camarón tempura y salmón flameado",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Rollos",
+      name: "Dragon Roll",
+      price: "L.349",
+      desc: "Anguila y tobikos",
+      image: "/images/placeholder.jpg",
+    },
+  
+  
+  
+    // RAMEN
+    {
+      category: "Ramen",
+      name: "Ramen",
+      price: "L.289",
+      desc: "Caldo japonés con ramen",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Ramen",
+      name: "Udon",
+      price: "L.289",
+      desc: "De pollo o camarón",
+      image: "/images/placeholder.jpg",
+    },
+  
+  
+  
+    // BOWLS
+    {
+      category: "Bowls",
+      name: "Teriyaki Chicken Bowl",
+      price: "L.259",
+      desc: "Pollo teriyaki con vegetales",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Bowls",
+      name: "Sweet Chili Chicken Bowl",
+      price: "L.249",
+      desc: "Pollo crispy sweet chili",
+      image: "/images/placeholder.jpg",
+    },
+  
+  
+  
+    // BEBIDAS
+    {
+      category: "Bebidas",
+      name: "Milk Tea Clasica",
+      price: "L.90",
+      desc: "Bubble tea clásica",
+      image: "/images/placeholder.jpg",
+    },
+  
+    {
+      category: "Bebidas",
+      name: "Soda de Fresa",
+      price: "L.70",
+      desc: "Refrescante bebida frutal",
+      image: "/images/placeholder.jpg",
+    },
+  
+  ];
+  
+  export default menu;
