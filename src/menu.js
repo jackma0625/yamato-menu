da68bdd5d1e8b890b3a3acb0e09d0f8cefc13b98
jackma0625 <jackma0625@gmail.com>
2026-06-21 21:19:57 -0600

    add camaron cantones

--- src/menu.js
+++ src/menu.js
@@ -1,367 +1,308 @@
 export const categories = [
+  {
+    category: "Entradas",
+    items: [
+      {
+        name: "Edamame",
+        desc: "Al vapor o asado",
+        image: "/images/edamame.webp",
+        options: [
+          { name: "Al vapor", price: 85 },
+          { name: "Asado", price: 110 },
+        ],
+      },
+      {
+        name: "Miso Soup",
+        desc: "Sopa japonesa con tofu y algas",
+        price: 120,
+        image: "/images/miso-soup.webp",
+      },
+      {
+        name: "Ensalada de Wakame",
+        desc: "Alga marina con sésamo",
+        price: 90,
+        image: "/images/ensalada-wakame.webp",
+      },
+      {
+        name: "Gyoza",
+        desc: "Pollo o cerdo · al vapor o frito",
+        price: 160,
+        image: "/images/gyoza.webp",
+      },
+      {
+        name: "Bao Asiática",
+        desc: "Pan al vapor relleno de cerdo BBQ",
+        price: 150,
+        image: "/images/bao.webp",
+      },
+      {
+        name: "Spring Roll",
+        desc: "Crujiente relleno de vegetales",
+        price: 170,
+        image: "/images/spring-roll.webp",
+      },
+      {
+        name: "Brocheta de Camaron",
+        desc: "2 / 5 piezas",
+        image: "/images/brocheta-camaron.webp",
+        options: [
+          { name: "De 2", price: 109 },
+          { name: "De 5", price: 229 },
+        ],
+      },
+      {
+        name: "Tempura Mixto",
+        desc: "Verduras y 3 piezas de camarones",
+        price: 249,
+        image: "/images/tempura.webp",
+      },
+      {
+        name: "Ika Sansai",
+        desc: "Ensalada de calamar",
+        price: 189,
+        image: "/images/ika-sansai.webp",
+      },
+      {
+        name: "Kids Chickens",
+        desc: "Crujiente de pollo acompañada de papas fritas",
+        price: 189,
+        image: "/images/kids-chicken.webp",
+      },
+    ],
+  },
+  {
+    category: "Rollos",
+    items: [
+      {
+        name: "Yamato Roll",
+        desc: "Camaron, aguacate, queso, zanahoria",
+        price: 249,
+        image: "/images/yamato.webp",
+      },
+      {
+        name: "Tokyo Roll",
+        desc: "Camaron, queso, aguacate, salmon, tobikos",
+        price: 300,
+        image: "/images/tokyo.webp",
+      },
+      {
+        name: "Ebi Roll",
+        desc: "Queso, aguacate, surimi, camaron",
+        price: 250,
+        image: "/images/ebi.webp",
+      },
+      {
+        name: "Salmon Lovers Roll",
+        desc: "Salmon y Aguacate",
+        price: 339,
+        image: "/images/salmon-lovers.webp",
+      },
+      {
+        name: "Sesame Roll",
+        desc: "Queso crema, camaron empanizado y aguacate",
+        price: 259,
+        image: "/images/sesame.webp",
+      },
+      {
+        name: "Sake Roll",
+        desc: "Salmon, queso crema, aguacate, cebollin",
+        price: 279,
+        image: "/images/sake.webp",
+      },
+      {
+        name: "Boston Roll",
+        desc: "Camarón tempura, surimi, Queso, Aguacate",
+        price: 250,
+        image: "/images/boston.webp",
+      },
+      {
+        name: "Dragon Roll",
+        desc: "Queso crema, camaron tempura, aguacate, anguila y tobikos",
+        price: 349,
+        image: "/images/dragon.webp",
+      },
+      {
+        name: "Zambo Roll",
+        desc: "Queso crema, camaron tempura, aguacate y platano",
+        price: 250,
+        image: "/images/zambo.webp",
+      },
+      {
+        name: "Kani Kama Roll",
+        desc: "Queso crema, aguacate y surimi",
+        price: 250,
+        image: "/images/kani.webp",
+      },
+      {
+        name: "Fuji Roll",
+        desc: "Queso crema, aguacate, salmon y tobikos",
+        price: 299,
+        image: "/images/fuji.webp",
+      },
+      {
+        name: "Sakura Roll",
+        desc: "Queso crema, aguacate, surimi y salmon",
+        price: 289,
+        image: "/images/sakura.webp",
+      },
+      {
+        name: "Nagoya Roll",
+        desc: "Queso crema, salmon y aguacate",
+        price: 280,
+        image: "/images/nagoya.webp",
+      },
+      {
+        name: "Okassan Roll",
+        desc: "Queso crema, pepino, surimi y salmon",
+        price: 280,
+        image: "/images/okassan.webp",
+      },
+      {
+        name: "California Roll",
+        desc: "aguacate, pepino y surimi",
+        price: 210,
+        image: "/images/california.webp",
+      },
+      {
+        name: "Nemo Roll",
+        desc: "Queso crema, aguacate, pepino y surimi",
+        price: 250,
+        image: "/images/nemo.webp",
+      },
+      {
+        name: "Alaska Roll",
+        desc: "Queso crema, surimi, pescado tempura y aguacate",
+        price: 250,
+        image: "/images/alaska.webp",
+      },
+      {
+        name: "Sofia Roll",
+        desc: "Queso crema, camaron tempura, aguacate, surimi y lechuga",
+        price: 250,
+        image: "/images/sofia.webp",
+      },
+      {
+        name: "Hokkaido Roll",
+        desc: "Queso crema, aguacate y surimi empanizado",
+        price: 220,
+        image: "/images/hokkaido.webp",
+      },
+      {
+        name: "Wakame Roll",
+        desc: "Queso crema, camaron tempura, aguacate y wakame",
+        price: 250,
+        image: "/images/wakame.webp",
+      },
+      {
+        name: "Queso Roll",
+        desc: "Queso crema, aguacate, pepino, surimi y queso americano",
+        price: 220,
+        image: "/images/queso.webp",
+      },
+      {
+        name: "Osaka Roll",
+        desc: "Pollo desmenuzado, Queso, aguacate y pepino.",
+        price: 250,
+        image: "/images/osaka.webp",
+      },
+      {
+        name: "Chicken Roll",
+        desc: "Queso crema, platano, pollo tempura y aguacate",
+        price: 250,
+        image: "/images/chicken.webp",
+      },
+      {
+        name: "Abokado Roll",
+        desc: "Queso crema y aguacate",
+        price: 220,
+        image: "/images/abokado.webp",
+      },
+      {
+        name: "Catracho Roll",
+        desc: "Queso crema, aguacate y platano",
+        price: 220,
+        image: "/images/catracho.webp",
+      },
+    ],
+  },
+  {
+    category: "Ramen",
+    items: [
+      {
+        name: "Ramen",
+        desc: "Ramen japonés en Copán con pollo o camarón",
+        price: 280,
+        image: "/images/ramen.webp",
+      },
+      {
+        name: "Udon",
+        desc: "Udon japonés estilo ramen en Copán",
+        price: 280,
+        image: "/images/udon.webp",
+      },
+    ],
+  },
+  {
+    category: "Bowls",
+    items: [
+      {
+        name: "Teriyaki Chicken Bowl",
+        desc: "Pollo teriyaki estilo japonés",
+        price: 250,
+        image: "/images/teriyaki-chicken.webp",
+      },
+      {
+        name: "Sweet Chili Chicken Bowl",
+        desc: "Pollo sweet chili estilo japonés",
+        price: 250,
+        image: "/images/chili-chicken.webp",
+      },
+    ],
+  },
 
-    {
-      category: "Entradas",
-  
-      items: [
-  
-        {
-          name: "Edamame",
-          desc: "Al vapor o asado",
-          image: "/images/edamame.webp",
-        
-          options: [
-            {
-              name: "Alvapor",
-              price: "85",
-            },
-        
-            {
-              name: "Asado",
-              price: "110",
-            },
-          ],
-        },
-  
-        {
-          name: "Miso Soup",
-          desc: "Sopa japonesa con tofu y algas",
-          price: "L.120",
-          image: "/images/miso-soup.webp",
-        },
-  
-        {
-          name: "Ensalada de Wakame",
-          desc: "Alga marina con sésamo",
-          price: "L.90",
-          image: "/images/ensalada-wakame.webp",
-        },
-  
-        {
-          name: "Gyoza",
-          desc: "Pollo o cerdo · al vapor o frito",
-          price: "L.160",
-          image: "/images/gyoza.webp",
-        },
-  
-        {
-          name: "Bao Asiática",
-          desc: "Pan al vapor relleno de cerdo BBQ",
-          price: "L.150",
-          image: "/images/bao.webp",
-        },
-  
-        {
-          name: "Spring Roll",
-          desc: "Crujiente relleno de vegetales",
-          price: "L.170",
-          image: "/images/spring-roll.webp",
-        },
-  
-        {
-          name: "Brocheta de Camaron",
-          desc: "2 / 5 piezas",
-          image: "/images/brocheta-camaron.webp",
-        
-          options: [
-            {
-              name: "De 2",
-              price: 109,
-            },
-        
-            {
-              name: "De 5",
-              price: 229,
-            },
-          ],
-        },
-  
-        {
-          name: "Tempura Mixto",
-          desc: "Verduras y 3 piezas de camarones",
-          price: "L.249",
-          image: "/images/tempura.webp",
-        },
-  
-        {
-          name: "Ika Sansai",
-          desc: "Ensalada de calamar",
-          price: "L.189",
-          image: "/images/ika-sansai.webp",
-        },
+  {
+    category: "Especialidades",
+    items: [
+      {
+        name: "Camaron Estilo Cantone",
+        desc: "camarones al vapor con jengible y cebollina",
+        image: "/images/camaron-cantones.webp",
+        options: [
+          { name: "18 unidades", price: 349 },
+          { name: "9 unidades", price: 180 },
+        ],
+      },
+      
+    ],
+  },
 
-        {
-          name: "Kids Chickens",
-          desc: "Crujiente de pollo acompanada de papas fritas",
-          price: "L.189",
-          image: "/images/kids-chicken.webp",
-        },
-  
-      ],
-    },
-  
-    {
-      category: "Rollos",
-  
-      items: [
-  
-        {
-          name: "Yamato Roll",
-          desc: "Camaron, aguacate, queso, zanahoria",
-          price: "L.249",
-          image: "/images/yamato.webp",
-        },
-  
-        {
-          name: "Tokyo Roll",
-          desc: "Camaron, queso, aguacate, salmon, tobikos",
-          price: "L.300",
-          image: "/images/tokyo.webp",
-        },
-  
-        {
-          name: "Ebi Roll",
-          desc: "Queso, aguacate, surimi, camaron",
-          price: "L.250",
-          image: "/images/ebi.webp",
-        },
-
-        {
-            name: "Salmon Lovers Roll",
-            desc: "Salmon y Aguacate",
-            price: "L.339",
-            image: "/images/salmon-lovers.webp",
-          },
-  
-          {
-            name: "Sesame Roll",
-            desc: "Queso crema, camaron empanizado y aguacate",
-            price: "L.259",
-            image: "/images/sesame.webp",
-          },
-
-          {
-            name: "Sake Roll",
-            desc: "Salmon, queso crema, aguacate, cebollin",
-            price: "L.279",
-            image: "/images/sake.webp",
-          },
-
-          {
-            name: "Boston Roll",
-            desc: "Camarón tempura, surimi,Queso, Aguacate",
-            price: "L.250",
-            image: "/images/boston.webp",
-          },
-
-          {
-            name: "Dragon Roll",
-            desc: "Queso crema, camaron tempura, aguacate, anguila y tobikos",
-            price: "L.349",
-            image: "/images/dragon.webp",
-          },
-
-          {
-            name: "Zambo Roll",
-            desc: "Queso crema, camaron tempura, aguacate y platano",
-            price: "L.250",
-            image: "/images/zambo.webp",
-          },
-
-          {
-            name: "Kani Kama Roll",
-            desc: "Queso crema, aguacate y surimi",
-            price: "L.250",
-            image: "/images/kani.webp",
-          },
-
-          {
-            name: "Fuji Roll",
-            desc: "Queso crema, aguacate, salmon y tobikos",
-            price: "L.299",
-            image: "/images/fuji.webp",
-          },
-
-          {
-            name: "Sakura Roll",
-            desc: "Queso crema, aguacate, surimi y salmon",
-            price: "L.289",
-            image: "/images/sakura.webp",
-          },
-
-          {
-            name: "Nagoya Roll",
-            desc: "Queso crema, salmon y agaucate",
-            price: "L.280",
-            image: "/images/nagoya.webp",
-          },
-
-          {
-            name: "Okassan Roll",
-            desc: "Queso crema, pepino, surimi y salmon",
-            price: "L.280",
-            image: "/images/okassan.webp",
-          },
-
-          {
-            name: "California Roll",
-            desc: "aguacate, pepino y surimi",
-            price: "L.210",
-            image: "/images/california.webp",
-          },
-
-          {
-            name: "Nemo Roll",
-            desc: "Queso crema, aguacate, pepino y surimi",
-            price: "L.250",
-            image: "/images/nemo.webp",
-          },
-
-          {
-            name: "Alaska Roll",
-            desc: "Queso crema, surimi, pescado tempura y aguacate",
-            price: "L.250",
-            image: "/images/alaska.webp",
-          },
-
-          {
-            name: "Sofia Roll",
-            desc: "Queso crema, camaron tempura, aguacate, surimi y lechuga",
-            price: "L.250",
-            image: "/images/sofia.webp",
-          },
-
-          {
-            name: "Hokkaido Roll",
-            desc: "Queso crema, aguacate y surimi empanizado",
-            price: "L.220",
-            image: "/images/hokkaido.webp",
-          },
-
-          {
-            name: "Wakame Roll",
-            desc: "Queso crema, camaron tempura, aguacate y wakame",
-            price: "L.250",
-            image: "/images/wakame.webp",
-          },
-
-          {
-            name: "Queso Roll",
-            desc: "Queso crema, aguacate, pepino, surimi y queso amricano",
-            price: "L.220",
-            image: "/images/queso.webp",
-          },
-
-          {
-            name: "Osaka Roll",
-            desc: "Pollo desmenuzado, Queso, aguacate y pepino.",
-            price: "L.250",
-            image: "/images/osaka.webp",
-          },
-
-          {
-            name: "Chicken Roll",
-            desc: "Queso crema, platano, pollo tempura y aguacate",
-            price: "L.250",
-            image: "/images/chicken.webp",
-          },
-
-          {
-            name: "Abokado Roll",
-            desc: "Queso crema y aguacate",
-            price: "L.220",
-            image: "/images/abokado.webp",
-          },
-
-          {
-            name: "Catracho Roll",
-            desc: "Queso crema, aguacate y platano",
-            price: "L.220",
-            image: "/images/catracho.webp",
-          },
-      ],
-    },
-  
-    {
-      category: "Ramen",
-  
-      items: [
-  
-        {
-          name: "Ramen",
-          desc: "Ramen japonés en Copán con pollo o camarón",
-          price: "L.280",
-          image: "/images/ramen.webp",
-        },
-  
-        {
-          name: "Udon",
-          desc: "Udon japonés estilo ramen en Copán",
-          price: "L.280",
-          image: "/images/udon.webp",
-        },
-  
-      ],
-    },
-  
-    {
-      category: "Bowls",
-  
-      items: [
-  
-        {
-          name: "Teriyaki Chicken Bowl",
-          desc: "Pollo teriyaki estilo japones",
-          price: "L.250",
-          image: "/images/teriyaki-chicken.webp",
-        },
-  
-        {
-          name: "Sweet Chili Chicken Bowl",
-          desc: "Pollo sweet chili estilo japones",
-          price: "L.250",
-          image: "/images/chili-chicken.webp",
-        },
-  
-      ],
-    },
-  
-    {
-      category: "Bubble Tea",
-  
-      items: [
-  
-        
-  
-        {
-          name: "Milk Tea Clasica",
-          desc: "Con Boba(tapioca,fresa,mango,manzana) +20",
-          price: "L.90",
-          image: "/images/milk-tea.webp",
-        },
-  
-        {
-          name: "Milk Tea Matcha",
-          desc: "Con Boba(tapioca,fresa,mango,manzana) +20",
-          price: "L.110",
-          image: "/images/milk-tea.webp",
-        },
-
-        {
-          name: "Milk Tea Fresa",
-          desc: "Con Boba(tapioca,fresa,mango,manzana) +20",
-          price: "L.110",
-          image: "/images/milktea-fresa.webp",
-        },
-
-        {
-          name: "Latte de Coco Frio",
-          desc: "estilo asiatico",
-          price: "L.90",
-          image: "/images/latte-coco.webp",
-        },
-  
-      ],
-    },
-  
-  ];
+  {
+    category: "Bubble Tea",
+    items: [
+      {
+        name: "Milk Tea Clásica",
+        desc: "Con Boba (tapioca, fresa, mango, manzana) +20",
+        price: 90,
+        image: "/images/milk-tea.webp",
+      },
+      {
+        name: "Milk Tea Matcha",
+        desc: "Con Boba (tapioca, fresa, mango, manzana) +20",
+        price: 110,
+        image: "/images/milk-tea.webp",
+      },
+      {
+        name: "Milk Tea Fresa",
+        desc: "Con Boba (tapioca, fresa, mango, manzana) +20",
+        price: 110,
+        image: "/images/milktea-fresa.webp",
+      },
+      {
+        name: "Latte de Coco Frío",
+        desc: "Estilo asiático",
+        price: 90,
+        image: "/images/latte-coco.webp",
+      },
+    ],
+  },
+];
